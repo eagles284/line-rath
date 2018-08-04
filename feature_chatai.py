62aca989e88abb56bd87705221fcde6fac57f5fc
probirds123@gmail.com <probirds123@gmail.com>
2018-08-04 19:05:23 +0700

    trombosit ai

--- feature_chatai.py
+++ feature_chatai.py
@@ -5,9 +5,12 @@ from chatterbot.trainers import ListTrainer
 # responds = open('chats.txt', 'r').readlines()
 # bot.train(responds)
 
-bot = ChatBot('Trombosit', storage_adapter='chatterbot.storage.SQLStorageAdapter')
+bot = ChatBot('Trombosit', storage_adapter='chatterbot.storage.SQLStorageAdapter', input_adapter="chatterbot.input.VariableInputTypeAdapter", output_adapter="chatterbot.output.OutputAdapter")
 bot.set_trainer(ListTrainer)
 
 def chat(input):
     response = bot.get_response(input)
     return response
+
+chatmess = "woi"
+print(chat(chatmess))
