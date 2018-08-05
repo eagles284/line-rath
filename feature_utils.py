9bec617fdb397ff844ca8b37d4d5b6c9fe9b40cb
probirds123@gmail.com <probirds123@gmail.com>
2018-08-05 16:20:28 +0700

    trombosit ai

--- feature_utils.py
+++ feature_utils.py
@@ -37,39 +37,39 @@ def google_news():
 # ================
 # Grafik Persamaan
 # ================
-# def plot():
-#     ############# Linear Equation of y = (3x - 4) / 2
+def plot():
+    ############# Linear Equation of y = (3x - 4) / 2
 
-#     # Create a dataframe with an x column containing values from -10 to 10
-#     # df = pd.DataFrame({'x': range(-10, 11)})
+    # Create a dataframe with an x column containing values from -10 to 10
+    # df = pd.DataFrame({'x': range(-10, 11)})
 
-#     # Define slope and y-intercept
-#     # m = 1.5
-#     # yInt = -2
+    # Define slope and y-intercept
+    # m = 1.5
+    # yInt = -2
 
-#     # Add a y column by applying the slope-intercept equation to x
-#     # df['y'] = m*df['x'] + yInt
-#     # print(df)
+    # Add a y column by applying the slope-intercept equation to x
+    # df['y'] = m*df['x'] + yInt
+    # print(df)
 
-#     # Plot the line
+    # Plot the line
 
-#     x = [0, 1]
-#     y = [1, 0]
+    x = [0, 1]
+    y = [1, 0]
 
-#     plt.plot(x, y, color="red")
-#     # plt.xlabel('x')
-#     # plt.ylabel('y')
-#     plt.axhline()
-#     plt.axvline()
-#     # plt.grid()
+    plt.plot(x, y, color="red")
+    # plt.xlabel('x')
+    # plt.ylabel('y')
+    plt.axhline()
+    plt.axvline()
+    # plt.grid()
 
-#     # label the y and x - intercept
+    # label the y and x - intercept
 
-#     # plot the slope from the y-intercept for 1x
-#     # mx = [0, 1]
-#     # my = [yInt, yInt + m]
-#     # plt.plot(mx,my, color='red', lw=5)
-#     plt.savefig('math.png')
+    # plot the slope from the y-intercept for 1x
+    # mx = [0, 1]
+    # my = [yInt, yInt + m]
+    # plt.plot(mx,my, color='red', lw=5)
+    plt.savefig('static/math.png')
     
     # plt.show()
 
