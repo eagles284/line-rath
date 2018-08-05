705b22ae6e9ffbc083478b417eea4465b5b297ba
probirds123@gmail.com <probirds123@gmail.com>
2018-08-05 17:47:53 +0700

    trombosit ai

--- feature_utils.py
+++ feature_utils.py
@@ -38,48 +38,78 @@ def google_news():
 # ================
 # Grafik Persamaan
 # ================
-def plot():
-    ############# Linear Equation of y = (3x - 4) / 2
+def plot(persamaan):
 
-    # Create a dataframe with an x column containing values from -10 to 10
-    # df = pd.DataFrame({'x': range(-10, 11)})
+    inputstring = str(persamaan)
 
-    # Define slope and y-intercept
-    # m = 1.5
-    # yInt = -2
+    if "x" in inputstring and "y" in inputstring and "=" in inputstring and "/" not in inputstring and "*" not in inputstring:
+        
+        try:
+            removex = inputstring.replace(" ", "").split("x")
+            removey = removex[1].split("y")
+            removee = removey[1].split("=")
 
-    # Add a y column by applying the slope-intercept equation to x
-    # df['y'] = m*df['x'] + yInt
-    # print(df)
+            xraw = int(removex[0])
+            yraw = int(removey[0])
+            e = int(removee[1])
 
-    # Plot the line
+            x = e/xraw
+            y = e/yraw
+            print("x:",xraw,"y:",yraw,"e:",e)
+            print("x:",x,"y:",y,"e:",e)
 
-    x = [0, 2]
-    y = [2, 0]
+            # Create a dataframe with an x column containing values from -10 to 10
+            # df = pd.DataFrame({'x': range(-10, 11)})
 
-    plt.plot(x, y, color="red")
-    # plt.xlabel('x')
-    # plt.ylabel('y')
-    plt.axhline()
-    plt.axvline()
-    # plt.grid()
+            # Define slope and y-intercept
+            # m = 1.5
+            # yInt = -2
 
-    # label the y and x - intercept
+            # Add a y column by applying the slope-intercept equation to x
+            # df['y'] = m*df['x'] + yInt
+            # print(df)
 
-    # plot the slope from the y-intercept for 1x
-    # mx = [0, 1]
-    # my = [yInt, yInt + m]
-    # plt.plot(mx,my, color='red', lw=5)
+            # Plot the line
 
-    datenowraw = str(datetime.datetime.now().time())
-    datenow = datenowraw.replace(":","")
+            xi = [0, x]
+            yi = [y, 0]
 
-    plt.savefig('static/' + datenow + ".png")
+            plt.plot(xi, yi, color="red")
+            # plt.xlabel('x')
+            # plt.ylabel('y')
+            plt.axhline()
+            plt.axvline()    
+            plt.grid()
 
-    fileurl = "https://trombosit.herokuapp.com/static/" + datenow + ".png"
+            strx = str(x)
+            stry = str(y)
 
-    return fileurl
-    # plt.show()
+            # label the y and x - intercept
+            plt.axvspan(x, y, facecolor='g', alpha=0)
+            plt.annotate(strx[0:5],(x,0-0.25), color='green')
+            plt.annotate(stry[0:5],(0+0.25,y), color='green')
+
+            # plot the slope from the y-intercept for 1x
+            # mx = [0, 1]
+            # my = [yInt, yInt + m]
+            # plt.plot(mx,my, color='red', lw=5)
+
+            datenowraw = str(datetime.datetime.now().time())
+            datenow = datenowraw.replace(":","")
+
+            plt.savefig('static/' + datenow + ".png")
+
+            fileurl = "https://trombosit.herokuapp.com/static/" + datenow + ".png"
+
+            # plt.show()  # REMOVE THIS ON EXECUTE!!!
+            return fileurl
+        except IndexError:
+            return
+        except Exception:
+            return
+    else:
+            print("Format error")
+            return
 
 # ==================
 # Screenshot Website
